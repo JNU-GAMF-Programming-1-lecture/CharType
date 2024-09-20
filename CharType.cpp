6fb78944122099aa4d096fe8dc6e98e899584580
Z. Csaba Johanyák <39920181+jzscsaba@users.noreply.github.com>
2024-09-20 21:33:51 +0200

    Add files via upload

---
+++ CharType.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	char c = 'A'; // the ASCII code of A is 65
+	i = c;
+	cout << "The code of letter " << c << " is: " << i << endl;
+	return 0;
+}
